4854a98cc4a99db3b7497c7e2321d21fc0c8b1e2
sem1234d <sem1234d@gmail.com>
2019-12-18 16:37:15 +0300

    Updates

---
+++ precache-manifest.16590855b9a08cdb58b44feffe182e11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297cbaf0a07c6ff54f39197c7a5a9c6c",
+    "url": "/zero_waste/index.html"
+  },
+  {
+    "revision": "f9a7ca0c172b05a38f03",
+    "url": "/zero_waste/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "d661f50a976756787abf",
+    "url": "/zero_waste/static/css/main.b654c5ab.chunk.css"
+  },
+  {
+    "revision": "f9a7ca0c172b05a38f03",
+    "url": "/zero_waste/static/js/2.e4802fc8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/zero_waste/static/js/2.e4802fc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d661f50a976756787abf",
+    "url": "/zero_waste/static/js/main.aa8b344d.chunk.js"
+  },
+  {
+    "revision": "039a458d4bc59302fb64",
+    "url": "/zero_waste/static/js/runtime-main.91fd8b46.js"
+  }
+]);
